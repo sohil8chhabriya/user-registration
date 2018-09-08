f9c9c58861b57e633fa3aa0589e86d4c5bb148af
sohilc <sohil8chhabriya@gmail.com>
2018-09-09 04:13:20 +0530

    applying webpack and other suppport - stable

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,48 @@
-module.exports = {
-  entry: "./src/app.js",
+const path = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const config = {
+  mode:"development",
+  entry: './src/index.js',
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: path.resolve('dist'),
+    filename: 'bundled.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
+	      include: path.resolve(__dirname, "src"),
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: { presets: ["es2015", "react"] }
+        loader: 'babel-loader',
+        query: { presets: [ 'es2015', 'react' ] },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true,
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
+module.exports = config;
